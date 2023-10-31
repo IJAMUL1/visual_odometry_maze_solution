e4e9ef99dcbb72c29a0115b178f05702189057b5
jesseinouye <jessinouye@gmail.com>
2023-10-31 18:01:13 -0400

    Added SuperGlue to test_matching.py

--- test_matching.py
+++ test_matching.py
@@ -1,7 +1,12 @@
 import cv2
 import os
 import numpy as np
+import matplotlib
+import matplotlib.cm as cm
+import torch
 
+from SuperGluePretrainedNetwork.models.matching import Matching
+from SuperGluePretrainedNetwork.models.utils import frame2tensor, make_matching_plot_fast
 
 class SLAM():
     def __init__(self,Cmat):        
@@ -90,7 +95,7 @@ class SLAM():
         good = []
         try:
             for m, n in matches:
-                if m.distance < 0.5 * n.distance:
+                if m.distance < 0.7 * n.distance:
                     good.append(m)
         except ValueError:
             pass
@@ -100,14 +105,14 @@ class SLAM():
                  matchesMask = None, # draw only inliers
                  flags = 2)
 
-        for g in good:
-            img3 = cv2.drawMatches(img_now, kp2, img_prev, kp1, [g], None, **draw_params)
-            cv2.imshow("image", img3)
-            key = cv2.waitKey(0)
+        # for g in good:
+        #     img3 = cv2.drawMatches(img_now, kp2, img_prev, kp1, [g], None, **draw_params)
+        #     cv2.imshow("image", img3)
+        #     key = cv2.waitKey(0)
         
-        # img3 = cv2.drawMatches(img_now, kp2, img_prev, kp1, good, None, **draw_params)
-        # cv2.imshow("image", img3)
-        # key = cv2.waitKey(0)
+        img3 = cv2.drawMatches(img_now, kp2, img_prev, kp1, good, None, **draw_params)
+        cv2.imshow("image", img3)
+        key = cv2.waitKey(0)
 
         # Check if the 'q' key is pressed (you can change 'q' to any key you prefer)
         # if key & 0xFF == ord('q'):
@@ -261,33 +266,148 @@ class SLAM():
 def extract_integer(filename):
     return int(filename.split('.')[0][5:])
 
+if 0:
+    K = np.array([[ 92.0, 0.0,  160.0],
+                [  0.0, 92.0, 120.0],
+                [  0.0,  0.0,   1.0]])
 
-K = np.array([[ 92.0, 0.0,  160.0],
-              [  0.0, 92.0, 120.0],
-              [  0.0,  0.0,   1.0]])
+    slam = SLAM(K)
 
-slam = SLAM(K)
+    filepath = "feature_test_images"
+    image_paths = [os.path.join(filepath, file) for file in sorted(os.listdir(filepath), key=extract_integer)]
+    imgs = [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
 
-filepath = "feature_test_images"
-image_paths = [os.path.join(filepath, file) for file in sorted(os.listdir(filepath), key=extract_integer)]
-imgs = [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
+    # print("len: {}".format(len(imgs)))
+    # print("type: {}".format(type(imgs[0])))
+    # print("shape: {}".format(imgs[0].shape))
 
-# print("len: {}".format(len(imgs)))
-# print("type: {}".format(type(imgs[0])))
-# print("shape: {}".format(imgs[0].shape))
-
-# cv2.imshow('img', imgs[0])
-# cv2.waitKey(0)
+    # cv2.imshow('img', imgs[0])
+    # cv2.waitKey(0)
 
 
-imgs.pop(0)
+    imgs.pop(0)
 
-img_now = imgs[42]
-img_prev = imgs[40]
+    img_now = imgs[100]
+    img_prev = imgs[103]
 
-# cv2.imshow('img_now', img_now)
-# cv2.waitKey(0)
-# cv2.imshow('img_prev', img_prev)
-# cv2.waitKey(0)
+    # cv2.imshow('img_now', img_now)
+    # cv2.waitKey(0)
+    # cv2.imshow('img_prev', img_prev)
+    # cv2.waitKey(0)
 
-slam.get_matches(img_now, img_prev)
+    slam.get_matches(img_now, img_prev)
+
+
+
+if 1:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    class Opt():
+        def __init__(self):
+            self.nms_radius = 4
+            self.keypoint_threshold = 0.005
+            self.max_keypoints = -1
+
+            self.superglue = 'indoor'
+            self.sinkhorn_iterations = 20
+            self.match_threshold = 0.2
+
+            self.show_keypoints = True
+
+    opt = Opt()
+
+    config = {
+        'superpoint': {
+            'nms_radius': opt.nms_radius,
+            'keypoint_threshold': opt.keypoint_threshold,
+            'max_keypoints': opt.max_keypoints
+        },
+        'superglue': {
+            'weights': opt.superglue,
+            'sinkhorn_iterations': opt.sinkhorn_iterations,
+            'match_threshold': opt.match_threshold,
+        }
+    }
+
+    matching = Matching(config).eval().to(device)
+    keys = ['keypoints', 'scores', 'descriptors']
+
+    filepath = "feature_test_images"
+    image_paths = [os.path.join(filepath, file) for file in sorted(os.listdir(filepath), key=extract_integer)]
+    imgs = [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
+
+    img1 = imgs[100]
+    img2 = imgs[103]
+    frame1_tensor = frame2tensor(img1, device)
+    frame2_tensor = frame2tensor(img2, device)
+
+    last_data = matching.superpoint({'image': frame1_tensor})
+    last_data = {k+'0': last_data[k] for k in keys}
+    last_data['image0'] = frame1_tensor
+    last_frame = img1
+    last_image_id = 0
+
+    pred = matching({**last_data, 'image1': frame2_tensor})
+    kpts0 = last_data['keypoints0'][0].cpu().numpy()
+    kpts1 = pred['keypoints1'][0].cpu().numpy()
+    matches = pred['matches0'][0].cpu().numpy()
+    confidence = pred['matching_scores0'][0].cpu().detach().numpy()
+    
+    print("matches:\n{}".format(matches))
+    print("kpts0:\n{}".format(matches))
+
+    valid = matches > -1
+    mkpts0 = kpts0[valid]
+    mkpts1 = kpts1[matches[valid]]
+    color = cm.jet(confidence[valid])
+    text = [
+        'SuperGlue',
+        'Keypoints: {}:{}'.format(len(kpts0), len(kpts1)),
+        'Matches: {}'.format(len(mkpts0))
+    ]
+    k_thresh = matching.superpoint.config['keypoint_threshold']
+    m_thresh = matching.superglue.config['match_threshold']
+    small_text = [
+        'Keypoint Threshold: {:.4f}'.format(k_thresh),
+        'Match Threshold: {:.2f}'.format(m_thresh),
+        'Image Pair: {:06}:{:06}'.format(1, 2),
+    ]
+
+    # print("matches:\n{}".format(matches))
+    # print("confidence:\n{}".format(confidence))
+    # print("kpts0:\n{}".format(kpts0))
+    # print("mkpts0:\n{}".format(mkpts0))
+
+
+    # ----------------------------
+    # for m0, m1 in zip(mkpts0, mkpts1):
+    #     out = make_matching_plot_fast(
+    #         img1, img2, kpts0, kpts1, [m0], [m1], color, text,
+    #         path=None, show_keypoints=opt.show_keypoints, small_text=small_text)
+        
+    #     cv2.imshow("matches", out)
+    #     cv2.waitKey(0)
+
+
+    # ----------------------------
+    idx = np.r_[0:1,15:16,25:26,30:31,40:41,50:51,60:61,70:71]
+
+    m0 = mkpts0[idx]
+    m1 = mkpts1[idx]
+
+    out = make_matching_plot_fast(
+        img1, img2, kpts0, kpts1, m0, m1, color, text,
+        path=None, show_keypoints=opt.show_keypoints, small_text=small_text)
+        
+    cv2.imshow("matches", out)
+    cv2.waitKey(0)
+
+
+
+    # ----------------------------
+    # out = make_matching_plot_fast(
+    #     img1, img2, kpts0, kpts1, mkpts0, mkpts1, color, text,
+    #     path=None, show_keypoints=opt.show_keypoints, small_text=small_text)
+    
+    # cv2.imshow("matches", out)
+    # cv2.waitKey(0)
