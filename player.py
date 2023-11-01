d62f0d6d41151bcd0e26bccf6a0a4f07a8ec0a5f
jesseinouye <jessinouye@gmail.com>
2023-11-01 17:34:27 -0400

    Refactoring player code

--- player.py
+++ player.py
@@ -15,18 +15,24 @@ from plot_path import visualize_paths
 from SuperGluePretrainedNetwork.models.matching import Matching
 from SuperGluePretrainedNetwork.models.utils import frame2tensor, make_matching_plot_fast
 
+START_STEP = 5
+STEP_SIZE = 4
+
+
+
+
 # SuperPoint / SuperGlue options
 class SuperOpt():
     def __init__(self):
         self.nms_radius = 4
         self.keypoint_threshold = 0.005
-        self.max_keypoints = -1
+        self.max_keypoints = 80
 
         self.superglue = 'indoor'
         self.sinkhorn_iterations = 20
-        self.match_threshold = 0.2
+        self.match_threshold = 0.3
 
-        self.show_keypoints = True
+        # self.show_keypoints = False
 
 
 # Player class
@@ -41,7 +47,7 @@ class KeyboardPlayerPyGame(Player):
         self.cur_pose = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
         super(KeyboardPlayerPyGame, self).__init__()
                 
-        self.img_count = 0  # Add a counter for image filenames
+        self.img_idx = 0  # Add a counter for image filenames
         self.last_save_time = time.time()  # Record the last time an image was saved
         self.all_fpv = []
 
@@ -64,8 +70,15 @@ class KeyboardPlayerPyGame(Player):
 
         self.matching = Matching(self.super_config).eval().to(self.device)
         self.super_keys = ['keypoints', 'scores', 'descriptors']
-        self.all_img_data = []
+        self.img_data_list = []
         self.img_tensor_list = []
+
+        self.prev_img_tensor = None
+
+        self.starting_step = START_STEP
+        self.step_size = STEP_SIZE
+
+        self.orb = cv2.ORB.create(1000)
 
 
     def reset(self):
@@ -145,112 +158,194 @@ class KeyboardPlayerPyGame(Player):
         super(KeyboardPlayerPyGame, self).set_target_images(images)
         self.show_target_images()
         
-                
 
+    # Find feature points using SuperPoint
+    def find_feature_points_superpoint(self, img):
+        img_tensor = frame2tensor(img, self.device)
+        img_data = self.matching.superpoint({'image': img_tensor})
+
+        self.img_data_list.append(img_data)
+        self.img_tensor_list.append(img_tensor)
+
+        return img_data['keypoints'], img_data['descriptors']
+
+
+    # Find feature points using ORB
+    def find_feature_points_orb(self, img):
+        pass
+
+
+    # Find feature matches using SuperGlue
+    def find_feature_matches_superglue(self, img1_index, img2_index):
+        img1_data = {k+'0':self.img_data_list[img1_index][k] for k in self.super_keys}
+        img1_data['image0'] = self.img_tensor_list[img1_index]
+
+        img2_data = {k+'1': self.img_data_list[img2_index][k] for k in self.super_keys}
+        img2_data['image1'] = self.img_tensor_list[img2_index]
+
+        pred = self.matching({**img1_data, **img2_data})
+        kpts0 = img1_data['keypoints0'][0].cpu().numpy()
+        kpts1 = img2_data['keypoints1'][0].cpu().numpy()
+        matches = pred['matches0'][0].cpu().numpy()
+        # confidence = pred['matching_scores0'][0].cpu().detach().numpy()
+
+        valid = matches > -1
+        mkpts0 = kpts0[valid]
+        mkpts1 = kpts1[matches[valid]]
+
+        # Get matching points (q1 for img1, q2 for img2)
+        q1 = np.array(mkpts0)
+        q2 = np.array(mkpts1)
+
+        return q1, q2
+
+
+    # Find pose
+    def find_pose(self, q1, q2):
+        # Get pose from SLAM class
+        relative_pose  = self.slam.get_pose(q1, q2)
+        relative_pose = np.nan_to_num(relative_pose, neginf=0, posinf=0)
+
+        # Save last x,z coordinates
+        prev_xz = (self.cur_pose[0,3], self.cur_pose[2,3])
+
+        # Calculate new pose from relative pose (transformation matrix)
+        self.cur_pose = np.matmul(self.cur_pose, np.linalg.inv(relative_pose))
+        # print("curr pose:\n{}".format(cur_pose))
+
+
+        # If not moving forward or backward, ignore the translation vector
+        # Translation vector seems to be normalized to 1 from decomposeEssentialMat()
+        # See: https://answers.opencv.org/question/66839/units-of-rotation-and-translation-from-essential-matrix/
+        if (self.last_act != Action.FORWARD) and (self.last_act != Action.BACKWARD):
+            self.cur_pose[0,3] = prev_xz[0]
+            self.cur_pose[2,3] = prev_xz[1]
+            
+        # Save current location
+        self.estimated_path.append((self.cur_pose[0,3], self.cur_pose[2,3]))
+
+        return self.cur_pose
+
+
+    # Process image
+    def process_image(self, fpv):
+        state = self.get_state()
+        if state is None:
+            return None
+        
+        step = state[2]
+
+        # If past starting step (to avoid static) and on a set interval (self.step_size)
+        if (step > self.starting_step) and ((step % self.step_size) == 0):
+            fpv_gray = cv2.cvtColor(fpv, cv2.COLOR_BGR2GRAY)
+            
+            # Find feature points
+            keypts, desc = self.find_feature_points_superpoint(fpv_gray)
+
+            # If more than one image processed (index >= 1)
+            if self.img_idx >= 1:
+
+                # Find feature matches between prev processed image and current image
+                q1, q2 = self.find_feature_matches_superglue(self.img_idx-1, self.img_idx)
+
+                pose = self.find_pose(q1, q2)
+
+            # Increment index of processed images
+            self.img_idx += 1
+
+        return True
+
+
+    # See (function used by game)
     def see(self, fpv):
         
         if fpv is None or len(fpv.shape) < 3:
             return
 
-        self.fpv = fpv        
-
-        # Check if 0.5 seconds have passed since the last image was saved
-        # current_time = time.time()
-        
-        # if current_time - self.last_save_time >= 0.01:
-        #     self.all_fpv.append(fpv) 
-        #     self.img_count += 1  # Increment the counter
-        #     self.last_save_time = current_time  # Update the last save time
+        self.fpv = fpv
                    
         if self.screen is None:
             h, w, _ = fpv.shape
             self.screen = pygame.display.set_mode((w, h))
+       
+        # Process image: find feature points, match feature points, get pose
+        # ret = self.process_image(fpv)
+
+
+        # ************************************
+        #      SAVE THIS STUFF BELOW
+        # ************************************
         
-        
-        # if self.img_count > 1:
-            
-        #     i = self.img_count-1
-            
-        #     # Perform template matching on consecutive images
-        #     img_now = self.all_fpv[i]
-        #     img_prev = self.all_fpv[i-1]      
-            
-        #     q1, q2 = self.slam.get_matches(img_now, img_prev)
-            
-        #     if len(q1) >=5:
-        #         # print("Q1 lenght =",len(q1))
-        #         # print("Q2 length = ",len(q2))
-        #         relative_pose = self.slam.get_pose(q1,q2)
-        #         relative_pose = np.nan_to_num(relative_pose, neginf=0,posinf=0)
-        #         self.estimated_path.append((self.cur_pose[0, 3], self.cur_pose[2, 3]))
-        #         self.cur_pose = np.matmul(self.cur_pose, np.linalg.inv(relative_pose))
-        #         print(self.estimated_path)            
-
-
-
         # SuperGlue implementation
         # -----------------------------------------------------
-        
-        # Interval for capturing / processing images
-        STEPSIZE = 3
+        # BEGIN IF 0
+        if 1:
+            # Interval for capturing / processing images
+            STEPSIZE = 4
 
-        state = self.get_state()
-        if state is not None:
-            step = state[2]
+            state = self.get_state()
+            if state is not None:
+                step = state[2]
 
-            # if current_time - self.last_save_time >= 0.1:
-            if (step > 5) and ((step % STEPSIZE) == 0):
-                fpv_gray = cv2.cvtColor(fpv, cv2.COLOR_BGR2GRAY)
-                img_tensor = frame2tensor(fpv_gray, self.device)
-                img_data = self.matching.superpoint({'image': img_tensor})
+                # if current_time - self.last_save_time >= 0.1:
+                if (step > 5) and ((step % STEPSIZE) == 0):
+                    fpv_gray = cv2.cvtColor(fpv, cv2.COLOR_BGR2GRAY)
+                    img_tensor = frame2tensor(fpv_gray, self.device)
+                    img_data = self.matching.superpoint({'image': img_tensor})
 
-                self.all_img_data.append(img_data)
-                self.img_tensor_list.append(img_tensor)
+                    self.img_data_list.append(img_data)
+                    self.img_tensor_list.append(img_tensor)
 
-                if self.img_count >= 1:
-                    img_prev_data = {k+'0':self.all_img_data[self.img_count-1][k] for k in self.super_keys}
-                    img_prev_data['image0'] = self.img_tensor_list[self.img_count-1]
+                    if self.img_idx >= 1:
+                        img_prev_data = {k+'0':self.img_data_list[self.img_idx-1][k] for k in self.super_keys}
+                        img_prev_data['image0'] = self.img_tensor_list[self.img_idx-1]
 
-                    img_now_data = {k+'1': img_data[k] for k in self.super_keys}
-                    img_now_data['image1'] = img_tensor
+                        img_now_data = {k+'1': img_data[k] for k in self.super_keys}
+                        img_now_data['image1'] = img_tensor
 
-                    d = {**img_prev_data, **img_now_data}
+                        # d = {**img_prev_data, **img_now_data}
 
-                    pred = self.matching({**img_prev_data, **img_now_data})
-                    kpts0 = img_prev_data['keypoints0'][0].cpu().numpy()
-                    kpts1 = img_now_data['keypoints1'][0].cpu().numpy()
-                    matches = pred['matches0'][0].cpu().numpy()
-                    # confidence = pred['matching_scores0'][0].cpu().detach().numpy()
+                        pred = self.matching({**img_prev_data, **img_now_data})
+                        kpts0 = img_prev_data['keypoints0'][0].cpu().numpy()
+                        kpts1 = img_now_data['keypoints1'][0].cpu().numpy()
+                        matches = pred['matches0'][0].cpu().numpy()
+                        # confidence = pred['matching_scores0'][0].cpu().detach().numpy()
 
-                    valid = matches > -1
-                    mkpts0 = kpts0[valid]
-                    mkpts1 = kpts1[matches[valid]]
+                        valid = matches > -1
+                        mkpts0 = kpts0[valid]
+                        mkpts1 = kpts1[matches[valid]]
 
-                    q1 = np.array(mkpts0)
-                    q2 = np.array(mkpts1)
+                        q1 = np.array(mkpts0)
+                        q2 = np.array(mkpts1)
 
-                    relative_pose  = self.slam.get_pose(q1, q2)
-                    relative_pose = np.nan_to_num(relative_pose, neginf=0, posinf=0)
+                        relative_pose  = self.slam.get_pose(q1, q2)
+                        relative_pose = np.nan_to_num(relative_pose, neginf=0, posinf=0)
 
-                    # Save last x,z coordinates
-                    prev_xz = (self.cur_pose[0,3], self.cur_pose[2,3])
+                        # Save last x,z coordinates
+                        prev_xz = (self.cur_pose[0,3], self.cur_pose[2,3])
 
-                    # Calculate new pose from relative pose (transformation matrix)
-                    self.cur_pose = np.matmul(self.cur_pose, np.linalg.inv(relative_pose))
-                    # print("curr pose:\n{}".format(cur_pose))
+                        # Calculate new pose from relative pose (transformation matrix)
+                        self.cur_pose = np.matmul(self.cur_pose, np.linalg.inv(relative_pose))
+                        # print("curr pose:\n{}".format(cur_pose))
 
 
-                    # If not moving forward or backward, ignore the translation vector
-                    # Translation vector seems to be normalized to 1 from decomposeEssentialMat()
-                    # See: https://answers.opencv.org/question/66839/units-of-rotation-and-translation-from-essential-matrix/
-                    if (self.last_act == Action.LEFT) or (self.last_act == Action.RIGHT):
-                        self.cur_pose[0,3] = prev_xz[0]
-                        self.cur_pose[2,3] = prev_xz[1]
-                        
-                    # Save current location
-                    self.estimated_path.append((self.cur_pose[0,3], self.cur_pose[2,3]))
+                        # If not moving forward or backward, ignore the translation vector
+                        # Translation vector seems to be normalized to 1 from decomposeEssentialMat()
+                        # See: https://answers.opencv.org/question/66839/units-of-rotation-and-translation-from-essential-matrix/
+                        if (self.last_act != Action.FORWARD) and (self.last_act != Action.BACKWARD):
+                            self.cur_pose[0,3] = prev_xz[0]
+                            self.cur_pose[2,3] = prev_xz[1]
+                            
+                        # Save current location
+                        self.estimated_path.append((self.cur_pose[0,3], self.cur_pose[2,3]))
 
-                self.img_count += 1
+                    # Update counters and previous data
+                    self.img_idx += 1
+        # END IF 0
+
+
+
+
     
 
         def convert_opencv_img_to_pygame(opencv_image):
