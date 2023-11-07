666b2984604bef400e0c0ce9ed76a61ec2cf3e66
jesseinouye <jessinouye@gmail.com>
2023-11-07 18:35:13 -0500

    Changed test_player to specify target set via command line

--- test_player.py
+++ test_player.py
@@ -1,16 +1,38 @@
 import os
+import sys
 import numpy as np
 import cv2 as cv2
 from player import KeyboardPlayerPyGame
 
 
 class TestPlayer(KeyboardPlayerPyGame):
-    def __init__(self):
+    def __init__(self, target_set=None):
+
+        self.test_target_dir = "test_targets"
+
+        if target_set is not None:
+            if not os.path.exists(self.test_target_dir):
+                print(f"Directory '{self.test_target_dir}' doesn't exist")
+                exit()
+
+            subdirs = [d for d in os.listdir(self.test_target_dir) if os.path.isdir(os.path.join(self.test_target_dir, d))]
+            
+            if target_set in subdirs:
+                self.target_set = target_set
+            else:
+                print(f"Directory for set '{target_set}' doesn't exist")
+                exit()
+        else:
+            self.target_set = "set_1"
+
+        print("---- TEST PLAYER ----")
+        print(f"Using test target set: {target_set}")
         super(TestPlayer, self).__init__()
         
 
     def get_target_images(self) -> list[np.ndarray]:
-        test_target_path = "test_targets/set_1"
+        test_target_path = self.test_target_dir + "/" + self.target_set
+        
 
         if self.img_idx < 5:
             return None
@@ -32,4 +54,10 @@ class TestPlayer(KeyboardPlayerPyGame):
 
 if __name__ == "__main__":
     import vis_nav_game
-    vis_nav_game.play(the_player=TestPlayer())
+
+    if len(sys.argv) > 1:
+        target_set = sys.argv[1]
+    else:
+        target_set = None
+
+    vis_nav_game.play(the_player=TestPlayer(target_set=target_set))
